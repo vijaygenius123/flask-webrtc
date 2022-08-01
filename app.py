aa3cdc7728c2aecdaa634cfade713a74c2b92ad7
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-08-01 12:52:56 +0100

    Add basic events

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@socketio.on_error_default
+def default_error_handler(e):
+    print("Error: {}".format(e))
+    socketio.stop()
+@socketio.on('join')
+def join(message):
+    username = message['username']
+    room = message['room']
+    join_room(room)
+    print('RoomEvent: {} has joined the room {}\n'.format(username, room))
+    emit('ready', {username: username}, to=room, skip_sid=request.sid)
+
+@socketio.on('data')
+def transfer_data(message):
+    username = message['username']
+    room = message['room']
+    data = message['data']
+    print('DataEvent: {} has sent the data:\n {}\n'.format(username, data))
+    emit('data', data, to=room, skip_sid=request.sid)
+
+
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, host="0.0.0.0", port=9000)
